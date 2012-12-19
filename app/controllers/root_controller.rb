8fd8ef799f67ffef4cc8f73f49eefb179c259efc
Jason <jason.addie@gmail.com>
2012-12-19 22:36:22 +0400

    moved story pages into visuals folder - preping for merge with ideas site

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,91 +4,10 @@ class RootController < ApplicationController
     @stories = Story.published.recent
     
     if params[:view] && params[:view] == 'list'
-      @view_type = 'list'
+      @view_type = 'visuals/list'
     else
-      @view_type = 'grid'
+      @view_type = 'visuals/grid'
     end
   end
 
-  def story
-    @story = Story.published.find(params[:id])
-  end
-
-  def vote
-		redirect_path = if request.env["HTTP_REFERER"]
-		    :back
-			else
-		    root_path
-			end
-
-    if !(['down', 'up'].include? params[:status])
-      redirect_to redirect_path, :alert => 'wrong status'
-      return
-    end
-
-    case params[:type]
-    when 'comment'
-      m = Comment
-    when 'story'
-      m = Story
-    else
-      redirect_to root_path, :alert => 'wrong votable'
-      return
-    end
-
-    ip = request.remote_ip
-    record = VoterIp.where(:ip => ip, :votable_type => params[:type], :votable_id => params[:votable_id])
-
-    if record.nil? || record.empty?
-
-      obj = m.find(params[:votable_id])
-      if obj.individual_votes.nil? || obj.individual_votes.length < 4
-        obj.individual_votes = '+0-0'
-      end
-
-      split = obj.individual_votes.split('+')[1].split('-')
-      ups = split[0].to_i
-      downs = split[1].to_i
-
-      if params[:status] == 'up'
-        ups = ups + 1
-      elsif params[:status] == 'down'
-        downs = downs + 1
-      end
-
-      obj.individual_votes = '+' + ups.to_s + '-' + downs.to_s
-			obj.overall_votes = ups - downs
-      obj.save
-
-      VoterIp.create(:ip => ip, :votable_type => params[:type], :votable_id => params[:votable_id], :status => params[:status])
-
-    elsif record[0].status != params[:status]
-
-      obj = m.find(params[:votable_id])
-
-      split = obj.individual_votes.split('+')[1].split('-')
-      ups = split[0].to_i
-      downs = split[1].to_i
-
-      if params[:status] == 'up'
-        ups = ups + 1
-        downs = downs - 1
-      elsif params[:status] == 'down'
-        ups = ups - 1
-        downs = downs + 1
-      end
-
-      obj.individual_votes = '+' + ups.to_s + '-' + downs.to_s
-			obj.overall_votes = ups - downs
-      obj.save
-
-      record[0].status = params[:status]
-      record[0].save
-    else
-	    redirect_to redirect_path
-      return false
-    end
-
-    redirect_to redirect_path
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,13 @@ BootstrapStarter::Application.routes.draw do
 		end
 
 
-		match '/story/:id', :to => 'root#story', :as => :story, :via => :get
-		match '/category/:id', :to => 'root#category', :as => :category, :via => :get
-		match '/search', :to => 'root#search', :as => :search, :via => :get
-		match '/search', :to => 'root#search', :as => :search, :via => :post
-		match '/vote/:type/:votable_id/:status', :to => 'root#vote', :as => :vote, :via => :get
-		match '/comment_notification/:idea_id', :to => 'root#comment_notification', :as => :comment_notification, :via => :get
+		match '/visualizations', :to => 'visuals#index', :as => :visuals, :via => :get
+		match '/visualizations/:id', :to => 'visuals#show', :as => :visual, :via => :get
+		match '/visualizations/category/:id', :to => 'visuals#category', :as => :category, :via => :get
+		match '/visualizations/search', :to => 'visuals#search', :as => :search, :via => :get
+		match '/visualizations/search', :to => 'visuals#search', :as => :search, :via => :post
+		match '/visualizations/vote/:type/:votable_id/:status', :to => 'visuals#vote', :as => :vote, :via => :get
+		match '/visualizations/comment_notification/:id', :to => 'visuals#comment_notification', :as => :comment_notification, :via => :get
 
 
 		root :to => 'root#index'

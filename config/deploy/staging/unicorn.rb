87e741454d8780ae0de30f6ada1e9505709c4475
Jason <jason.addie@gmail.com>
2012-12-24 15:04:55 +0400

    added staging deploy info

--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -1,9 +1,9 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/bootstrap-staging/Bootstrap-Starter-Staging/current" # path to application current folder
-sock_name = "unicorn_Bootstrap-Starter-Staging" # must be unique name with no '.'
-port_num = 8090 # must be a unique port number for this application
+root = "/home/news-staging/Visualizing-News-Staging/current" # path to application current folder
+sock_name = "unicorn_Visualizing-News-Staging" # must be unique name with no '.'
+port_num = 8104 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
 

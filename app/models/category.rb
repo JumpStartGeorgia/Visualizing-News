5fdacbd5d81aff6604ee7cfbf6bf15a2d7fbfc8b
Jason <jason.addie@gmail.com>
2012-12-27 23:10:43 +0400

    added category icons

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,7 +11,7 @@ class Category < ActiveRecord::Base
     :path => ":rails_root/public/system/category/:attachment/:id/:filename"
 
   accepts_nested_attributes_for :category_translations
-  attr_accessible :id, :icon, :category_translations_attributes
+  attr_accessible :id, :icon, :category_translations_attributes, :icon_content_type, :icon_file_size, :icon_updated_at, :icon_file_name
 
   def permalink
     Utf8Converter.convert_ka_to_en(self.name.downcase.gsub(" ","_").gsub("/","_").gsub("__","_").gsub("__","_"))

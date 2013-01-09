def1d560700bc081b6f55aab8b0bd603e10612bb
Jason <jason.addie@gmail.com>
2013-01-09 10:47:27 +0400

    fixed encoding error for file attachment; changed locale key name for email subject; trying to send email from user

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,22 +4,26 @@ class ContactMailer < ActionMailer::Base
 
   def new_message(message)
     @message = message
-    mail(:cc => "#{message.name} <#{message.email}>", :subject => I18n.t("mailer.contact.subject"))
+    mail(:from => "#{message.name} <#{message.email}>",
+			:cc => "#{message.name} <#{message.email}>",
+			:subject => I18n.t("mailer.contact.contact_form.subject"))
   end
 
   def send_data(message)
     @message = message
-    
+
     # if a file was provided, add it as an attachment
     file = nil
     if @message.file
-      file = Tempfile.new("send_data_attachment_#{Time.now.strftime("%Y%m%dT%H%M%S%z")}")
+      file = Tempfile.new("send_data_attachment_#{Time.now.strftime("%Y%m%dT%H%M%S%z")}", :encoding => 'ascii-8bit')
       file.write(@message.file.read)
 			file.rewind
       attachments[@message.file.original_filename] = file.read
     end
-    
-    mail(:cc => "#{message.name} <#{message.email}>", :subject => I18n.t("mailer.send_data.subject"))
+
+    mail(:from => "#{message.name} <#{message.email}>",
+			:cc => "#{message.name} <#{message.email}>",
+			:subject => I18n.t("mailer.contact.send_data.subject"))
 
     # kill the temp attachment file
     file.close if file
@@ -28,7 +32,9 @@ class ContactMailer < ActionMailer::Base
 
   def submit_visual(message)
     @message = message
-    mail(:cc => "#{message.name} <#{message.email}>", :subject => I18n.t("mailer.submit_visual.subject"))
+    mail(:from => "#{message.name} <#{message.email}>",
+			:cc => "#{message.name} <#{message.email}>",
+			:subject => I18n.t("mailer.contact.submit_visual.subject"))
   end
 
 end

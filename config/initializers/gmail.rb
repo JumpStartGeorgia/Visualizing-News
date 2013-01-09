761f6cac93f23e0865b457511c479a94cb8464bf
Jason <jason.addie@gmail.com>
2013-01-09 11:36:57 +0400

    missed a setting

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -2,7 +2,7 @@ if Rails.env.production? || Rails.env.staging?
 	ActionMailer::Base.smtp_settings = {
 		:address              => "smtp.gmail.com",
 		:port                 => 587,
-		:domain               => 'www.jumpstart.ge',
+		:domain               => 'www.feradi.info',
 		:user_name            => ENV['FERADI_FEEDBACK_TO_EMAIL'],
 		:password             => ENV['FERADI_FEEDBACK_FROM_PWD'],
 		:authentication       => 'plain',

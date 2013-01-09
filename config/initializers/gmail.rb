829d38215d1a5f809b1da8eba4934de6c04d6544
Jason <jason.addie@gmail.com>
2013-01-09 11:39:04 +0400

    reseting back to jumpstart.ge

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -2,9 +2,9 @@ if Rails.env.production? || Rails.env.staging?
 	ActionMailer::Base.smtp_settings = {
 		:address              => "smtp.gmail.com",
 		:port                 => 587,
-		:domain               => 'www.feradi.info',
-		:user_name            => ENV['FERADI_FEEDBACK_TO_EMAIL'],
-		:password             => ENV['FERADI_FEEDBACK_FROM_PWD'],
+		:domain               => 'www.jumpstart.ge',
+		:user_name            => ENV['APPLICATION_FEEDBACK_FROM_EMAIL'],
+		:password             => ENV['APPLICATION_FEEDBACK_FROM_PWD'],
 		:authentication       => 'plain',
 		:enable_starttls_auto => true
 	}

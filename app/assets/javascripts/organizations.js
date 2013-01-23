73c478d2c6d9d8ee1242dc4e29e51c2fc0b88044
Jason <jason.addie@gmail.com>
2013-01-23 23:49:10 +0400

    added styling for org show page category toggle

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -3,7 +3,22 @@ $(function ()
 
   $('#toggle_categories').click(function ()
   {
-    $('#category_slider').slideToggle();
+    // rotate the arrow 
+    deg = '0';
+    ie = '0';
+    if ($('#category_slider').css('display') == 'none'){
+      deg = '180';
+      ie = '2';
+    }
+    $('#category_slider').slideToggle(400);
+    $('#category_arrow').fadeOut(200, function(){
+      $('#category_arrow').css('-webkit-transform', 'rotate(' + deg + 'deg)');
+      $('#category_arrow').css('-moz-transform', 'rotate(' + deg + 'deg)');
+      $('#category_arrow').css('-ms-transform', 'rotate(' + deg + 'deg)');
+      $('#category_arrow').css('-o-transform', 'rotate(' + deg + 'deg)');
+      $('#category_arrow').css('filter', 'progid:DXImageTransform.Microsoft.BasicImage(rotation=' + ie + ')');
+      $('#category_arrow').fadeIn(200);
+    })
   });
 
 

cf9c3b076d02f93301d64d5ec1c804dd385b7e82
jason <jason.addie@gmail.com>
2013-01-31 20:28:08 +0400

    added category to the language switcher checker

--- app/models/category_translation.rb
+++ app/models/category_translation.rb
@@ -1,8 +1,15 @@
 class CategoryTranslation < ActiveRecord::Base
+	require 'utf8_converter'
 	belongs_to :category
 
   attr_accessible :category_id, :name, :locale
 
   validates :name, :presence => true
+
+
+  def permalink
+    Utf8Converter.convert_ka_to_en(self.name.downcase.gsub(" ","_").gsub("/","_").gsub("__","_").gsub("__","_"))
+  end
+
 
 end

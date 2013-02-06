f88cbae8fbb97bef3004c3c51d24fa2aa14e5838
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-02-06 22:26:29 +0400

    fancybox back to ajax. added a check to prevent conflicts. like tested with registering a new user, working

--- app/assets/javascripts/fancybox2.js
+++ app/assets/javascripts/fancybox2.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+  function focus_fancybox_input ()
+  {
+    if (gon.highlight_first_form_field)
+    {
+      $("#fancybox-content :input:visible:enabled:first").focus();
+    }
+  }
+
 	// if a link with 'fancybox-nested' class is clicked, get the html and replace with current html
 	function fancybox_nested_links() {
 		$.get($(this).attr('href'), function(data){
@@ -9,26 +17,23 @@ $(document).ready(function(){
 			$('#fancybox-content div.content').html(x);
 			// register any new fancybox-nested links
 			$('#fancybox-content a.fancybox-nested').click(fancybox_nested_links);
+			focus_fancybox_input();
 		});
 
 		return false;
 	}
 
 	// register click function for 'fancybox-nested' class that should only be used on pages that are opened in fancybox
-	$('#fancybox-content a.fancybox-nested').click(fancybox_nested_links);
+	$('#fancybox-content a.fancybox-nested').live('click', fancybox_nested_links);
 
 	// register fancybox
 	$("a.fancybox").fancybox({
     transitionIn: 'elastic',
     transitionOut: 'elastic',
 	  width: 400,
-	  type: 'iframe',
 		onComplete: function ()
 		{
-	    if (gon.highlight_first_form_field)
-	    {
-	      $("#fancybox-content :input:visible:enabled:first").focus();
-	    }
+		  focus_fancybox_input();
 	  }
   });
 	$("a.fancybox_visual").fancybox({

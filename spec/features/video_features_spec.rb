2c139fabdd98060084312f77e35951a08504606d
nwshane <django09@gmail.com>
2015-11-16 11:19:21 +0400

    Made test for video embed code on show page pass.

--- spec/features/video_features_spec.rb
+++ spec/features/video_features_spec.rb
@@ -6,7 +6,22 @@ RSpec.describe 'Video', type: :feature do
   end
 
   it 'embed code is visible on show page' do
-    visit visualization_path(video_visualization.reload.permalink)
+    video_visualization.update_column(:published, true)
+    video_visualization.update_column(:published_date, Time.now)
+    c = Category.create
+    c.name = 'test category 1'
+    c.save!
+    video_visualization.categories << c
+
+    video_visualization.organization.name = 'Test organization 1'
+    video_visualization.organization.save!
+    ot = video_visualization.organization.translations.first
+    ot.permalink = Utf8Converter.convert_ka_to_en(ot.name).to_ascii.downcase!.gsub!(' ', '-')
+    ot.save!
+
+    video_visualization.reload
+
+    visit visualization_path(video_visualization.permalink)
 
     expect(page).to have_content('VIDEO EMBED CODE RUNS HERE')
   end

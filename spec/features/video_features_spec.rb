70ae114d2b3e197b30a213f8afeda9abf01162b8
nwshane <django09@gmail.com>
2015-11-16 11:49:41 +0400

    Added documentation comment to video visualization reload in video features spec to clarify reload's purpose.

--- spec/features/video_features_spec.rb
+++ spec/features/video_features_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Video', type: :feature do
   end
 
   it 'embed code is visible on show page' do
+    # Register the visualization translations to enable calling permalink
     video_visualization.reload
 
     visit visualization_path(video_visualization.permalink)

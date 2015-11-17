d43d951e014314f96d24a72af226557bc59650ef
nwshane <django09@gmail.com>
2015-11-17 12:13:48 +0400

    Added example image to upload on new visualization form, and finished spec that creates a visualization.

--- spec/features/video_features_spec.rb
+++ spec/features/video_features_spec.rb
@@ -33,7 +33,31 @@ RSpec.describe 'Video', type: :feature, js: true do
 
     visit new_organization_visualization_path(organization.reload.permalink)
     expect(page).to have_content('New Visualization')
-    # visit visualization form
-    # fill everything out
+
+    within '#fields-panel' do
+      choose 'Infographic'
+
+      # select languages
+      find(:css, '#visualization_languages_internal_en').set(true)
+      find(:css, '#visualization_languages_internal_hy').set(false)
+      find(:css, '#visualization_languages_internal_ka').set(false)
+    end
+
+    within '#form-en' do
+      fill_in 'Title', with: 'test-video-vis'
+      attach_file 'visualization_visualization_translations_attributes_0_image_file_attributes_file',
+                  Rails.root.join('spec', 'example_files', 'how-dependent-is-georgia-on-russia-economically_en_medium.jpg')
+
+    end
+
+
+    # Add video url
+    # Upload visual image
+
+    click_on 'Create Visualization'
+    expect(page).to have_content 'Visualization was successfully created.'
+
+    # expect video url to be something
+    # expect video embed to be something
   end
 end

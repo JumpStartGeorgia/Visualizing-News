89127764322a2230a8109b7f7b649c6007bb9476
nwshane <django09@gmail.com>
2015-11-18 12:48:32 +0400

    Added validation for video visualization to check that video_url is not blank.

---
+++ spec/models/visualization_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Visualization', type: :model do
+  context 'when type is video' do
+    describe 'translation #video_url' do
+      it 'causes error if blank' do
+        video = FactoryGirl.create(:video_visualization).reload
+        translation = video.translations[0]
+        translation.video_url = nil
+        translation.save!
+
+        expect(video.errors.messages.length).to eq(1)
+      end
+    end
+  end
+end

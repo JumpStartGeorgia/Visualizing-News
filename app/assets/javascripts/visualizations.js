cd5b1be222ee00fe2e050c0b0197bb3fdbad26e3
nwshane <django09@gmail.com>
2015-11-18 11:20:36 +0400

    Refactored set_video_embed_code by extracting out two other functions.

--- app/assets/javascripts/visualizations.js
+++ app/assets/javascripts/visualizations.js
@@ -106,6 +106,7 @@ $(document).ready(function(){
 
 		$('.js-generate-video-embed').change(function() {
 			set_video_embed_code(this);
+			$('.js')
 		});
 
 		// if type changes, show appropriate fields
@@ -200,9 +201,17 @@ $(document).ready(function(){
 
 });
 
+function get_video_url_input_val(container) {
+	return $(container).find('input').val();
+}
+
+function find_video_embed_sibling(element) {
+	return $(element).siblings('.js-receive-video-embed')[0];
+}
+
 function set_video_embed_code(video_url_div) {
-	video_url = $(video_url_div).find('input').val();
-	video_embed_div = $(video_url_div).siblings('.js-receive-video-embed')[0];
+	video_url = get_video_url_input_val(video_url_div);
+	video_embed_div = find_video_embed_sibling(video_url_div);
 
 	olly.embed(video_url, video_embed_div);
 }

95d1c15f8839dbe1a439cda1f1fd6192b518c453
nwshane <django09@gmail.com>
2015-11-19 12:05:40 +0400

    Removed commented out code in image_file factory.

--- spec/factories/image_files.rb
+++ spec/factories/image_files.rb
@@ -1,15 +1,5 @@
 FactoryGirl.define do
   factory :image_file do
     visualization_translation
-    #
-    # file do
-    #   File.new(
-    #     Rails.root.join(
-    #       'spec',
-    #       'example_files',
-    #       'how-dependent-is-georgia-on-russia-economically_en_medium.jpg'
-    #     )
-    #   )
-    # end
   end
 end

f55658e54a092f63f1502d839eedb13373d57538
nwshane <django09@gmail.com>
2015-11-19 12:16:00 +0400

    Wrote empty visualization translation validation specs.

---
+++ spec/models/visualization_translation_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Visualization Translation', type: :model do
+  context 'with valid fields' do
+    it 'is valid' do
+
+    end
+  end
+
+  describe '#video_url' do
+    it 'causes error if has no http' do
+
+    end
+  end
+
+  describe '#video_embed' do
+    context 'with blank video url' do
+      it 'does not cause error if blank' do
+
+      end
+    end
+    context 'with present video url' do
+      it 'causes error if blank' do
+
+      end
+    end
+  end
+end

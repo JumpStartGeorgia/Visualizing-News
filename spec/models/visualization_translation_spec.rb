3316b8a768eaa18b3331f86128ed809d36e21e15
nwshane <django09@gmail.com>
2015-11-19 12:36:09 +0400

    Nested visualization translation specs for video in 'when vis type is video' namespace.

--- spec/models/visualization_translation_spec.rb
+++ spec/models/visualization_translation_spec.rb
@@ -1,29 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Visualization Translation', type: :model do
-  context 'with valid fields' do
-    it 'is valid' do
-      vt = FactoryGirl.create(:visualization_translation)
+  context 'when type of visualization is video' do
+    context 'with valid fields' do
+      it 'is valid' do
+        vt = FactoryGirl.create(:video_visualization_translation)
 
-      expect(vt.valid?).to eq(true)
-    end
-  end
-
-  describe '#video_url' do
-    it 'causes error if has no http' do
-
-    end
-  end
-
-  describe '#video_embed' do
-    context 'with blank video url' do
-      it 'does not cause error if blank' do
-
+        expect(vt.valid?).to eq(true)
       end
     end
-    context 'with present video url' do
-      it 'causes error if blank' do
 
+    describe '#video_url' do
+      it 'causes error if does not contain protocol' do
+        
+      end
+    end
+
+    describe '#video_embed' do
+      context 'with blank video url' do
+        it 'does not cause error if blank' do
+
+        end
+      end
+      context 'with present video url' do
+        it 'causes error if blank' do
+
+        end
       end
     end
   end

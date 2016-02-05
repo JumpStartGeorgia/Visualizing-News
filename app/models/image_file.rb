c37f684ad6687527fd9493f361a38050a521bbc6
nwshane <django09@gmail.com>
2016-02-05 12:44:33 +0400

    Code style fixes in image_file model.

--- app/models/image_file.rb
+++ app/models/image_file.rb
@@ -1,101 +1,101 @@
 class ImageFile < ActiveRecord::Base
-   belongs_to :visualization_translation
+  belongs_to :visualization_translation
 
-	  attr_accessible :visualization_translation_id, :visualization_type_id,
-  			              :file, :file_file_name, :file_content_type, :file_file_size, :file_updated_at,
-  			              :crop_x, :crop_y, :crop_w, :crop_h, :reset_crop, :image_is_cropped, :redid_crop, :reload_file
+  attr_accessible :visualization_translation_id, :visualization_type_id,
+                      :file, :file_file_name, :file_content_type, :file_file_size, :file_updated_at,
+                      :crop_x, :crop_y, :crop_w, :crop_h, :reset_crop, :image_is_cropped, :redid_crop, :reload_file
 
-	  attr_accessor :reset_crop, :was_cropped, :redid_crop, :reload_file
+  attr_accessor :reset_crop, :was_cropped, :redid_crop, :reload_file
 
   # validates :file_file_name, :presence => true, :if => "visualization_type_id != Visualization::TYPES[:interactive]"
 
-	 has_attached_file :file,
+  has_attached_file :file,
                     url: '/system/visualizations/:visual_id/image/:permalink_:locale_:style.:extension',
-                		  styles: proc { |attachment| attachment.instance.attachment_styles }
-	 #:convert_options => {
+                    styles: proc { |attachment| attachment.instance.attachment_styles }
+  #:convert_options => {
   #     :thumb => "-gravity north -thumbnail 230x230^ -extent 230x230"
   # },
 
-	 # if this is a new record, do not apply the cropping processor
-	 # - the user must be able to set the crop size first
-	 def attachment_styles
- 		 styles = {}
- 		 if visualization_type_id == Visualization::TYPES[:infographic]
-  			 if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
-   				 styles = {
-    					 thumb: { geometry: '230x230#' },
-    					 medium: { geometry: '600x>' },
-    					 large: { geometry: '900x>' }
-    				}
-   			else
-   				 styles = {
-    					 thumb: { geometry: '230x230#', processors: [:cropper] }
-    				}
-   			end
-  		elsif visualization_type_id == Visualization::TYPES[:interactive]
-  			 if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
-   				 styles = {
-    					 thumb: { geometry: '230x230#' },
-    					 medium: { geometry: '600x>', convert_options: '-gravity northwest -thumbnail 600x500^ -extent 600x500' },
-    					 large: { geometry: '900x>' }
-          #					:large => {:geometry => "900x>", :convert_options => "-gravity northwest -thumbnail 900x500^ -extent 900x500"}
-    				}
-   			else
-   				 styles = {
-    					 thumb: { geometry: '230x230#', processors: [:cropper] }
-    				}
-   			end
-  		elsif visualization_type_id == Visualization::TYPES[:fact]
-  			 if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
-   				 styles = {
-    					 thumb: { geometry: '230x230#' },
-    					 medium: { geometry: '600x>' },
-    					 large: { geometry: '900x>' }
-    				}
-   			else
-   				 styles = {
-    					 thumb: { geometry: '230x230#', processors: [:cropper] }
-    				}
-   			end
-  		elsif visualization_type_id == Visualization::TYPES[:comic]
-  			 if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
-   				 styles = {
-    					 thumb: { geometry: '230x230#' },
-    					 medium: { geometry: '600x>' },
-    					 large: { geometry: '900x>' }
-    				}
-   			else
-   				 styles = {
-    					 thumb: { geometry: '230x230#', processors: [:cropper] }
-    				}
-   			end
-  		elsif visualization_type_id == Visualization::TYPES[:video]
-  			 if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
-   				 styles = {
-    					 thumb: { geometry: '230x230#' },
-    					 medium: { geometry: '600x>' },
-    					 large: { geometry: '900x>' }
-    				}
-   			else
-   				 styles = {
-    					 thumb: { geometry: '230x230#', processors: [:cropper] }
-    				}
-   			end
-  		end
+  # if this is a new record, do not apply the cropping processor
+  # - the user must be able to set the crop size first
+  def attachment_styles
+    styles = {}
 
- 		 styles
- 	end
+    if visualization_type_id == Visualization::TYPES[:infographic]
+      if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
+        styles = {
+          thumb: { geometry: '230x230#' },
+          medium: { geometry: '600x>' },
+          large: { geometry: '900x>' }
+        }
+      else
+        styles = {
+          thumb: { geometry: '230x230#', processors: [:cropper] }
+        }
+      end
+    elsif visualization_type_id == Visualization::TYPES[:interactive]
+      if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
+        styles = {
+          thumb: { geometry: '230x230#' },
+          medium: { geometry: '600x>', convert_options: '-gravity northwest -thumbnail 600x500^ -extent 600x500' },
+          large: { geometry: '900x>' }
+        }
+      else
+        styles = {
+          thumb: { geometry: '230x230#', processors: [:cropper] }
+        }
+      end
+    elsif visualization_type_id == Visualization::TYPES[:fact]
+      if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
+        styles = {
+          thumb: { geometry: '230x230#' },
+          medium: { geometry: '600x>' },
+          large: { geometry: '900x>' }
+        }
+      else
+        styles = {
+          thumb: { geometry: '230x230#', processors: [:cropper] }
+        }
+      end
+    elsif visualization_type_id == Visualization::TYPES[:comic]
+      if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
+        styles = {
+          thumb: { geometry: '230x230#' },
+          medium: { geometry: '600x>' },
+          large: { geometry: '900x>' }
+        }
+      else
+        styles = {
+             thumb: { geometry: '230x230#', processors: [:cropper] }
+        }
+      end
+    elsif visualization_type_id == Visualization::TYPES[:video]
+      if id.nil? || reload_file || crop_x.nil? || crop_y.nil? || crop_w.nil? || crop_h.nil?
+        styles = {
+          thumb: { geometry: '230x230#' },
+          medium: { geometry: '600x>' },
+          large: { geometry: '900x>' }
+        }
+      else
+        styles = {
+          thumb: { geometry: '230x230#', processors: [:cropper] }
+        }
+      end
+    end
+
+    styles
+  end
 
   # with new version of paperclip, can no longer run this as after update because
   # paperclip updates the file updated date after reprocessing, thus causing infinite loop
   # - reprocess_file now called in edit controller
   #  after_update :reprocess_file, :if => :cropping?
 
-	 after_find :set_flags
+   after_find :set_flags
 
-	 def set_flags
- 		 self.was_cropped = image_is_cropped
- 	end
+   def set_flags
+      self.was_cropped = image_is_cropped
+   end
 
   def cropping?
     image_is_cropped && !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
@@ -107,7 +107,7 @@ class ImageFile < ActiveRecord::Base
   end
 
   # private
-	 def reprocess_file
- 		 file.reprocess!
- 	end
+   def reprocess_file
+      file.reprocess!
+   end
 end

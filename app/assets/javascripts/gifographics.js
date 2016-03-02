6b9c47682bceae0a619e1885796df68c0dc52147
nwshane <django09@gmail.com>
2016-03-02 10:56:32 +0400

    Added new function to load gifographic from placeholder.

--- app/assets/javascripts/gifographics.js
+++ app/assets/javascripts/gifographics.js
@@ -96,14 +96,17 @@ function create_gif_image_from_placeholder($placeholder) {
 	return gif;
 }
 
+function load_gifographic_from_placeholder($placeholder) {
+	var gifographic_image = create_gif_image_from_placeholder($placeholder);
+
+	$(gifographic_image).one('load', function(){
+		setup_gifographic(gifographic_image);
+		replace_placeholder_with_gifographic($placeholder, gifographic_image);
+	});
+}
+
 function setup_gifographics() {
 	$gifographics().each(function() {
-		var $placeholder = $(this);
-		var gifographic_image = create_gif_image_from_placeholder($placeholder);
-
-		$(gifographic_image).one('load', function(){
-			setup_gifographic(gifographic_image);
-			replace_placeholder_with_gifographic($placeholder, gifographic_image);
-		});
+		load_gifographic_from_placeholder($(this));
 	});
 }

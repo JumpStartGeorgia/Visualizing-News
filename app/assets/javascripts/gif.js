fdcb1fb8a1471ca317577de2075fb97135c15a24
nwshane <django09@gmail.com>
2016-03-09 10:14:24 +0400

    Removed old global freeze_gif function.

--- app/assets/javascripts/gif.js
+++ app/assets/javascripts/gif.js
@@ -26,26 +26,6 @@ function show_cover_image(gif_image) {
 	gif_cover_image(gif_image).removeClass('is-hidden');
 }
 
-function freeze_gif(i) {
-	if (this.is_playable()) {
-		$(i).addClass('is-frozen');
-		set_play_title(i);
-		show_cover_image(i);
-	}
-
-	var c = document.createElement('canvas');
-	var w = c.width = i.width;
-	var h = c.height = i.height;
-	c.getContext('2d').drawImage(i, 0, 0, w, h);
-	try {
-		i.src = c.toDataURL("image/gif"); // if possible, retain all css aspects
-	} catch(e) { // cross-domain -- mimic original with all its tag attributes
-		for (var j = 0, a; a = i.attributes[j]; j++)
-			c.setAttribute(a.name, a.value);
-		i.parentNode.replaceChild(c, i);
-	}
-}
-
 function create_gif(gif_image) {
 	var image = gif_image;
 

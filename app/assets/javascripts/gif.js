8f6e0e5bb1bdc9f561271ccab6f414eadec1eea4
nwshane <django09@gmail.com>
2016-03-09 10:29:00 +0400

    Adding methods to gif object after its creation.

--- app/assets/javascripts/gif.js
+++ app/assets/javascripts/gif.js
@@ -29,68 +29,68 @@ function create_gif(gif_image) {
 		gif_cover_image().removeClass('is-hidden');
 	}
 
-	var gif = {
-		cover_image: function() {
-			return gif_cover_image();
-		},
+	var gif = {}
 
-    play: function() {
-      $(image).removeClass('is-frozen');
-      set_stop_title(image);
-      hide_cover_image();
+	gif.cover_image = function() {
+		return gif_cover_image();
+	}
 
-      set_src_to_data_src(image);
-    },
+	gif.play = function() {
+		$(image).removeClass('is-frozen');
+		set_stop_title(image);
+		hide_cover_image();
 
-		freeze: function() {
-			if (this.is_playable()) {
-				$(image).addClass('is-frozen');
-				set_play_title(image);
-				show_cover_image();
-			}
+		set_src_to_data_src(image);
+	}
 
-			var c = document.createElement('canvas');
-			var w = c.width = image.width;
-			var h = c.height = image.height;
-			c.getContext('2d').drawImage(image, 0, 0, w, h);
-			try {
-				image.src = c.toDataURL("image/gif"); // if possible, retain all css aspects
-			} catch(e) { // cross-domain -- mimic original with all its tag attributes
-				for (var j = 0, a; a = image.attributes[j]; j++)
-					c.setAttribute(a.name, a.value);
-				image.parentNode.replaceChild(c, image);
-			}
-		},
-
-		make_playable_by: function(element) {
-			var that = this;
-
-			$(element).click(function() {
-				if ($(image).hasClass('is-frozen')) {
-					that.play();
-				} else {
-					that.freeze();
-				}
-			});
-		},
-
-		is_playable: function() {
-			return $(image).hasClass('js-gif-is-playable');;
-		},
-
-		add_to_dom_after: function($element) {
-			$element.after(image);
-		},
-
-		make_playable: function() {
-			this.make_playable_by(image);
-		},
-
-		on_load_do: function(callback) {
-			$(image).one('load', function() {
-				callback();
-			});
+	gif.freeze = function() {
+		if (this.is_playable()) {
+			$(image).addClass('is-frozen');
+			set_play_title(image);
+			show_cover_image();
 		}
+
+		var c = document.createElement('canvas');
+		var w = c.width = image.width;
+		var h = c.height = image.height;
+		c.getContext('2d').drawImage(image, 0, 0, w, h);
+		try {
+			image.src = c.toDataURL("image/gif"); // if possible, retain all css aspects
+		} catch(e) { // cross-domain -- mimic original with all its tag attributes
+			for (var j = 0, a; a = image.attributes[j]; j++)
+				c.setAttribute(a.name, a.value);
+			image.parentNode.replaceChild(c, image);
+		}
+	}
+
+	gif.make_playable_by = function(element) {
+		var that = this;
+
+		$(element).click(function() {
+			if ($(image).hasClass('is-frozen')) {
+				that.play();
+			} else {
+				that.freeze();
+			}
+		});
+	}
+
+	gif.is_playable = function() {
+		return $(image).hasClass('js-gif-is-playable');;
+	}
+
+	gif.add_to_dom_after = function($element) {
+		$element.after(image);
+	}
+
+	gif.make_playable = function() {
+		this.make_playable_by(image);
+	}
+
+	gif.on_load_do = function(callback) {
+		$(image).one('load', function() {
+			callback();
+		});
 	}
 
 	return gif;

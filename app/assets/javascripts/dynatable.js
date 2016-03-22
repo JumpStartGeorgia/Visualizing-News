35dabe8e9c49873c1d831477c9ec6b41a75f8290
nwshane <django09@gmail.com>
2016-03-22 14:00:35 +0400

    Almost finished implementing export button.

--- app/assets/javascripts/dynatable.js
+++ app/assets/javascripts/dynatable.js
@@ -5,5 +5,53 @@ function createDynatable(selector) {
 
   var data = $(selector).dynatable();
 
+  function get_headers() {
+    var headers = $(selector).find('thead th').map(function() {
+      return $(this).text().replace('▼', '').trim();
+    });
+
+    return $.makeArray(headers);
+  }
+
+  function surround_with_strings(str) {
+    return '"' + str + '"';
+  }
+
+  function make_exportable() {
+    var export_button_id = $(selector).data('exportableById');
+    var $export_button = $('#' + export_button_id);
+
+    $export_button.click(function() {
+      var $table = $(selector);
+
+      $table.data('dynatable').records.resetOriginal();
+      $table.data('dynatable').queries.run();
+      $table.data('dynatable').sorts.init();
+      var nodes = $table.data('dynatable').records.sort();
+
+      var csvContent = "data:text/csv;charset=utf-8,";
+      var headers_str = get_headers().map(surround_with_strings).join(',');
+      csvContent += headers_str + "\n";
+
+      nodes.forEach(function(infoArray, index){
+        infoArray = $.map(infoArray, function(el) {
+          if (typeof el === 'string') {
+            el = '"' + el.trim() + '"';
+          }
+
+          return el;
+        });
+
+        dataString = infoArray.join(",");
+        csvContent += index < nodes.length ? dataString+ "\n" : dataString;
+      });
+
+      var encodedUri = encodeURI(csvContent);
+      window.open(encodedUri);
+    });
+  }
+
+  make_exportable();
+
   return true;
 }

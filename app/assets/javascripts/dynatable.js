165d3a654faf6d4061e6a75a8ff9cc19da8bfeba
nwshane <django09@gmail.com>
2016-03-23 11:01:34 +0400

    Refactored dynatable JS code.

--- app/assets/javascripts/dynatable.js
+++ app/assets/javascripts/dynatable.js
@@ -29,38 +29,42 @@ function createDynatable(selector) {
     return '"' + str + '"';
   }
 
+  function export_table() {
+    var $table = $(selector);
+
+    $table.data('dynatable').records.resetOriginal();
+    $table.data('dynatable').queries.run();
+    $table.data('dynatable').sorts.init();
+    var nodes = $table.data('dynatable').records.sort();
+
+    var csvContent = "data:text/csv;charset=utf-8,";
+    var headers_str = get_headers().map(surround_with_strings).join(',');
+    csvContent += headers_str + "\n";
+
+    nodes.forEach(function(infoArray, index){
+      infoArray = $.map(infoArray, function(el) {
+        if (typeof el === 'string') {
+          el = '"' + el.trim() + '"';
+        }
+
+        return el;
+      });
+
+      infoArray.shift();
+      dataString = infoArray.join(",");
+      csvContent += index < nodes.length ? dataString+ "\n" : dataString;
+    });
+
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+  }
+
   function make_exportable() {
     var export_button_id = $(selector).data('exportableById');
     var $export_button = $('#' + export_button_id);
 
     $export_button.click(function() {
-      var $table = $(selector);
-
-      $table.data('dynatable').records.resetOriginal();
-      $table.data('dynatable').queries.run();
-      $table.data('dynatable').sorts.init();
-      var nodes = $table.data('dynatable').records.sort();
-
-      var csvContent = "data:text/csv;charset=utf-8,";
-      var headers_str = get_headers().map(surround_with_strings).join(',');
-      csvContent += headers_str + "\n";
-
-      nodes.forEach(function(infoArray, index){
-        infoArray = $.map(infoArray, function(el) {
-          if (typeof el === 'string') {
-            el = '"' + el.trim() + '"';
-          }
-
-          return el;
-        });
-
-        infoArray.shift();
-        dataString = infoArray.join(",");
-        csvContent += index < nodes.length ? dataString+ "\n" : dataString;
-      });
-
-      var encodedUri = encodeURI(csvContent);
-      window.open(encodedUri);
+      export_table();
     });
   }
 

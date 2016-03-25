2a02924545d567b1d597c1f930270ff545f1b4e0
nwshane <django09@gmail.com>
2016-03-25 13:12:12 +0400

    Made engagement table queries more readable.

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -8,8 +8,24 @@ class Admin::AnalyticsController < ApplicationController
     @visualizations = Visualization.published.with_translations
 
     # No way to SUM multiple columns in ActiveRecord query language
-    @categories = Visualization.select('category_translations.name AS name, visualizations.languages, SUM(visualizations.impressions_count) AS impressions_count, SUM(visualizations.fb_likes) AS fb_likes, SUM(visualizations.overall_votes) AS overall_votes').joins(categories: :category_translations).where("category_translations.locale = '#{I18n.locale}'").group('name')
+    @categories = (
+      Visualization.select(
+        "category_translations.name AS name, \
+        visualizations.languages, \
+        SUM(visualizations.impressions_count) AS impressions_count, \
+        SUM(visualizations.fb_likes) AS fb_likes, \
+        SUM(visualizations.overall_votes) AS overall_votes")
+                   .joins(categories: :category_translations)
+                   .where("category_translations.locale = '#{I18n.locale}'")
+                   .group('name'))
 
-    @visualization_types = Visualization.select('visualizations.visualization_type_id, visualizations.languages, SUM(visualizations.impressions_count) AS impressions_count, SUM(visualizations.fb_likes) AS fb_likes, SUM(visualizations.overall_votes) AS overall_votes').group(:visualization_type_id)
+    @visualization_types = (
+      Visualization.select(
+        "visualizations.visualization_type_id, \
+        visualizations.languages, \
+        SUM(visualizations.impressions_count) AS impressions_count, \
+        SUM(visualizations.fb_likes) AS fb_likes, \
+        SUM(visualizations.overall_votes) AS overall_votes")
+                   .group(:visualization_type_id))
   end
 end

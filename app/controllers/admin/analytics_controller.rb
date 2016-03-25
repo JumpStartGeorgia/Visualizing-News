3ff62bb590d631dfa19e6eb7f9983a11de94e506
nwshane <django09@gmail.com>
2016-03-25 12:36:14 +0400

    Wrote simplified first version of query for analytics engagement by category table.

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -6,6 +6,7 @@ class Admin::AnalyticsController < ApplicationController
 
   def index
     @visualizations = Visualization.published.with_translations
+    @categories = Visualization.select('categories.icon_file_name AS name, visualizations.languages, SUM(visualizations.impressions_count) AS impressions_count').joins(:categories).group('name')
 
     @visualization_types = Visualization.select('visualizations.visualization_type_id, visualizations.languages, SUM(visualizations.impressions_count) AS impressions_count, SUM(visualizations.fb_likes) AS fb_likes, SUM(visualizations.overall_votes) AS overall_votes').group(:visualization_type_id)
   end

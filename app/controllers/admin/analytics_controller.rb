ce13587d68623cebb9f24ad7335df0cc50bb5eca
nwshane <django09@gmail.com>
2016-03-25 11:30:58 +0400

    Add analytics engagement table by type.

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -6,5 +6,7 @@ class Admin::AnalyticsController < ApplicationController
 
   def index
     @visualizations = Visualization.published.with_translations
+
+    @visualization_types = Visualization.select('visualizations.visualization_type_id AS id, visualizations.languages, SUM(visualizations.impressions_count) AS impressions_count, SUM(visualizations.fb_likes) AS fb_likes, SUM(visualizations.overall_votes) AS overall_votes').group(:visualization_type_id)
   end
 end

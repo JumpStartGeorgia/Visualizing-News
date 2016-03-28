d7181961ac9558e7555c63a1315625065aaf9d62
nwshane <django09@gmail.com>
2016-03-28 16:56:18 +0400

    Separated display of different engagement tables on analytics index.

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -5,27 +5,38 @@ class Admin::AnalyticsController < ApplicationController
   end
 
   def index
-    @visualizations = Visualization.published.with_translations
+    @engagement_table = params['engagement_table'] ||= 'visuals'
 
-    # No way to SUM multiple columns in ActiveRecord query language
-    @categories = (
-      Visualization.select(
-        "category_translations.name AS name, \
-        visualizations.languages, \
-        SUM(visualizations.impressions_count) AS impressions_count, \
-        SUM(visualizations.fb_likes) AS fb_likes, \
-        SUM(visualizations.overall_votes) AS overall_votes")
-                   .joins(categories: :category_translations)
-                   .where('category_translations.locale = ?', I18n.locale)
-                   .group('name'))
+    case @engagement_table
+    when 'visuals'
 
-    @visualization_types = (
-      Visualization.select(
-        "visualizations.visualization_type_id, \
-        visualizations.languages, \
-        SUM(visualizations.impressions_count) AS impressions_count, \
-        SUM(visualizations.fb_likes) AS fb_likes, \
-        SUM(visualizations.overall_votes) AS overall_votes")
-                   .group(:visualization_type_id))
+      @table_rows = Visualization.published.with_translations
+
+    when 'categories'
+
+      # No way to SUM multiple columns in ActiveRecord query language
+      @table_rows = (
+        Visualization.select(
+          "category_translations.name AS name, \
+          visualizations.languages, \
+          SUM(visualizations.impressions_count) AS impressions_count, \
+          SUM(visualizations.fb_likes) AS fb_likes, \
+          SUM(visualizations.overall_votes) AS overall_votes")
+                     .joins(categories: :category_translations)
+                     .where('category_translations.locale = ?', I18n.locale)
+                     .group('name'))
+
+    when 'visual_types'
+
+      @table_rows = (
+        Visualization.select(
+          "visualizations.visualization_type_id, \
+          visualizations.languages, \
+          SUM(visualizations.impressions_count) AS impressions_count, \
+          SUM(visualizations.fb_likes) AS fb_likes, \
+          SUM(visualizations.overall_votes) AS overall_votes")
+                     .group(:visualization_type_id))
+
+    end
   end
 end
